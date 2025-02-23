500c5e22ad32302a9cc7c22ff7cc9c88bad8a2ef
Karim Tantawy <kareemtantawy01@gmail.com>
2025-02-23 23:26:13 +0200

    added bookshelf app

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -3,3 +3,7 @@ from .models import Book
 # Register your models here.
 # Book.site.register(Book)
 admin.ModelAdmin(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
