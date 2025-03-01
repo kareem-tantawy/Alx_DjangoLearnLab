d0947acaea69c4e17e9fd0156c8e75db561ff4de
Karim Tantawy <kareemtantawy01@gmail.com>
2025-03-01 14:38:54 +0200

    Added django models

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_books, name='home'),
-    path('library/', views.library_detail, name='library'),
+    path('library/<int:pk>/', views.library_detail, name='library_detail')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
+from .models import Book, Author, Library, Librarian
 
 # Create your views here.
 
 def list_books(request):
+    books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html')
 
 def library_detail(request):
-    return render(request, 'relationship_app/library_detail.html')
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    # return render(request, 'relationship_app/library_detail.html')
