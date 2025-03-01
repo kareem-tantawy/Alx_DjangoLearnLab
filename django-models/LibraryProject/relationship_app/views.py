939fa080b40d35afd674eb5c1d51a6c5268396d6
Karim Tantawy <kareemtantawy01@gmail.com>
2025-03-01 15:03:29 +0200

    Added django models

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,20 +3,23 @@ from .models import Library, Book, Author, Librarian
 from django.views.generic.detail import DetailView
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
+
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html')
+    return render(request, "relationship_app/list_books.html")
+
 
 def LibraryDetailView(request):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library'
+    template_name = "library_detail.html"
+    context_object_name = "library"
     # return render(request, 'relationship_app/library_detail.html')
-    
+
+
 # User Login View
 def user_login(request):
     if request.method == "POST":
@@ -29,10 +32,12 @@ def user_login(request):
         form = AuthenticationForm()
     return render(request, "relationship_app/login.html", {"form": form})
 
+
 # User Logout View
 def user_logout(request):
     logout(request)
     return render(request, "relationship_app/logout.html")
+
 
 # User Registration View
 def user_register(request):
