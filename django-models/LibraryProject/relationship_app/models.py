fd85e0f1813deb776973f2c4412163d8368f92f8
Karim Tantawy <kareemtantawy01@gmail.com>
2025-03-01 15:25:48 +0200

    Added django models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+# Role Choices
+ROLE_CHOICES = [
+    ("Admin", "Admin"),
+    ("Librarian", "Librarian"),
+    ("Member", "Member"),
+]
 
 # Create your models here.
+
 
 class Author(models.Model):
     name = models.CharField(max_length=50)
@@ -8,7 +19,8 @@ class Author(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     pages = models.IntegerField()
@@ -16,11 +28,33 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-    
+
+
 class Library(models.Model):
     name = models.CharField(max_length=100)
     book = models.ManyToManyField(Book)
-    
+
+
 class Librarian(models.Model):
     name = models.CharField(max_length=50)
-    library = models.OneToOneField('Library', on_delete=models.CASCADE)
+    library = models.OneToOneField("Library", on_delete=models.CASCADE)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="Member")
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+
+
+# Automatically create a UserProfile when a new User is registered
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
